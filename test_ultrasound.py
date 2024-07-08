cf416b169a352122178e6239810faf10cdb08ce5
ValeriaCampagna <valeria.campagna1@gmail.com>
2024-07-07 21:17:16 -0400

    Add delay for test code

--- test_ultrasound.py
+++ test_ultrasound.py
@@ -1,7 +1,9 @@
+import time
 import serial
 
 sonic_ser = serial.Serial('/dev/arduinoUltrasound', 57600, timeout=1)
 
 while True:
     print(sonic_ser.readline().decode("utf-8"), end='')
+    time.sleep(.5)
 
