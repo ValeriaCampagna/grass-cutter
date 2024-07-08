03190f4446a4678fa818f4957c6e27ef850eafa2
ValeriaCampagna <valeria.campagna1@gmail.com>
2024-07-07 22:22:09 -0400

    Ultra sound test now working

--- test_ultrasound.py
+++ test_ultrasound.py
@@ -1,9 +1,8 @@
 import time
 import serial
 
-sonic_ser = serial.Serial('/dev/arduinoUltrasound', 57600, timeout=1)
+sonic_ser = serial.Serial('/dev/ttyUSB0', 57600, timeout=1)
 
+time.sleep(2)
 while True:
-    print(sonic_ser.readline().decode("utf-8"), end='')
-    time.sleep(.5)
-
+    print(sonic_ser.readline().decode("utf-8").strip())
