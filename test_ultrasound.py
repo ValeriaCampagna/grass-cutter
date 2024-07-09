1fcfae8345ba9fad8cba9bc44f86f271b4f0c3ce
ValeriaCampagna <valeria.campagna1@gmail.com>
2024-07-08 20:23:28 -0400

    Adupte testi codi

--- test_ultrasound.py
+++ test_ultrasound.py
@@ -4,9 +4,10 @@ import serial
 sonic_ser = serial.Serial('/dev/arduinoUltrasound', 57600, timeout=1)
 
 time.sleep(2)
+sonic_ser.flushInput()
+
 while True:
     try:
-        sonic_ser.flushInput()
         if sonic_ser.in_waiting > 0:
             print(sonic_ser.readline().decode("utf-8"))
     except KeyboardInterrupt:
