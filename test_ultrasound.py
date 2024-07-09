cfdde3db9898967e84c6a2f9e4ec621242dc474a
ValeriaCampagna <valeria.campagna1@gmail.com>
2024-07-08 20:21:02 -0400

    Update test code again

--- test_ultrasound.py
+++ test_ultrasound.py
@@ -6,8 +6,10 @@ sonic_ser = serial.Serial('/dev/arduinoUltrasound', 57600, timeout=1)
 time.sleep(2)
 while True:
     try:
-        print(sonic_ser.readline().decode("utf-8"))
+        sonic_ser.flushInput()
+        if sonic_ser.in_waiting > 0:
+            print(sonic_ser.readline().decode("utf-8"))
     except KeyboardInterrupt:
         sonic_ser.close()
-        break
         print("closing")
+        break
