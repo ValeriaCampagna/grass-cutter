cb2c9e280d957abbf87cf4169f8e5e17a1e7d8db
ValeriaCampagna <valeria.campagna1@gmail.com>
2024-07-13 16:05:12 -0400

    Update sensors test code

--- test_all_arduinos.py
+++ test_all_arduinos.py
@@ -1,22 +1,22 @@
 import time
 import serial
 
+
 def test_serial(port, baudrate, name):
-    try:
-        ser = serial.Serial(port, baudrate, timeout=1)
-        time.sleep(3)
-        ser.flushInput()
-        while True:
-            if ser.in_waiting > 0:
-                line = ser.readline().decode('utf-8').strip()
-                print(f"{name}: {line}")
-            time.sleep(0.1)
-    except serial.SerialException as e:
-        print(f"Error opening serial port {name}: {e}")
-    finally:
-        ser.close()
+    ser = serial.Serial(port, baudrate, timeout=1)
+    time.sleep(3)
+    ser.flushInput()
+    return ser
+
 
 # Test each device independently
-test_serial('/dev/arduinoUltrasound', 57600, 'Ultrasound')
-# test_serial('/dev/arduinoSensors', 115200, 'Angle')
+ultra = test_serial('/dev/arduinoUltrasound', 57600, 'Ultrasound')
+sensors = test_serial('/dev/arduinoSensors', 115200, 'Angle')
 # test_serial('/dev/arduinoMotors', 115200, 'Motor')
+
+while True:
+    line = ultra.readline().decode('utf-8').strip()
+    print(f"ultrasound: {line}")
+    gline = sensors.readline().decode('utf-8').strip()
+    print(f"angles: {gline}")
+    time.sleep(0.1)
