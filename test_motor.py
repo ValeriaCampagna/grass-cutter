925115040235ca04d6544dfbb22a818506595ef9
ValeriaCampagna <valeria.campagna1@gmail.com>
2024-07-14 16:45:28 -0400

    Update test motor code

--- test_motor.py
+++ test_motor.py
@@ -3,13 +3,17 @@ import serial
 
 motor_ser = serial.Serial('/dev/arduinoMotors', 115200, timeout=1)
 
-while True:
-    speed = 90
-    for i in range(20):
-        command = f"{speed},{speed}\n"
-        motor_ser.write(command.encode())
-        speed = min(150, speed+4)
-        time.sleep(0.1)
-    time.sleep(4)
-    motor_ser.write("0,0\n".encode())
-    time.sleep(4)
+try:
+    while True:
+        speed = 70
+        for i in range(20):
+            command = f"{speed},{speed}\n"
+            motor_ser.write(command.encode())
+            speed = min(90, speed+1)
+            print("current speed:", speed)
+            time.sleep(0.2)
+        time.sleep(4)
+        motor_ser.write("0,0\n".encode())
+        time.sleep(4)
+except KeyboardInterrupt:
+    motor_ser.write("-10,-10\n".encode())
