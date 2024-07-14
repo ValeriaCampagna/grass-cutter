a5ed59b475a2646323b108e2396109265226fa65
ValeriaCampagna <valeria.campagna1@gmail.com>
2024-07-14 17:13:29 -0400

    blah

--- test_motor.py
+++ test_motor.py
@@ -4,6 +4,8 @@ import serial
 motor_ser = serial.Serial('/dev/arduinoMotors', 115200, timeout=1)
 angles_ser = serial.Serial('/dev/arduinoSensors', 115200, timeout=1)
 
+time.sleep(2)
+
 steps = 0
 try:
     while True:
@@ -11,13 +13,15 @@ try:
         for i in range(20):
             command = f"{speed},{speed}\n"
             motor_ser.write(command.encode())
-            speed = min(155, speed+8)
+            speed = min(185, speed+10)
             print("current speed:", speed)
             time.sleep(0.2)
+
         for i in range(100):
             vals = angles_ser.readline().decode("utf-8").strip()
             try:
                 _, lenc, renc = vals.split(",")
+                print("encoder left", vals)
             except Exception:
                 print("Error reading")
                 time.sleep(0.1)
