42909b87b5e500639e2012135ccb1dd8bd67f344
ValeriaCampagna <valeria.campagna1@gmail.com>
2024-07-14 16:57:38 -0400

    Better motor test

--- test_motor.py
+++ test_motor.py
@@ -2,11 +2,23 @@ import time
 import serial
 
 motor_ser = serial.Serial('/dev/arduinoMotors', 115200, timeout=1)
+angles_ser = serial.Serial('/dev/arduinoSensors', 115200, timeout=1)
 
+steps = 0
 try:
     while True:
         speed = 0
         for i in range(20):
+            vals = angles_ser.readline().decode("utf-8").strip()
+            try:
+                _, lenc, renc = vals.split(",")
+            except Exception:
+                print("Error reading")
+                time.sleep(0.1)
+            if lenc > 5760:
+                print("already moving")
+                speed = 90
+                break
             command = f"{speed},{speed}\n"
             motor_ser.write(command.encode())
             speed = min(155, speed+8)
