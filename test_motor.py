ea037a155cf39d2734689898ca871572c46abf51
ValeriaCampagna <valeria.campagna1@gmail.com>
2024-07-14 17:20:00 -0400

    Update ser

--- test_motor.py
+++ test_motor.py
@@ -17,18 +17,21 @@ try:
         #     print("current speed:", speed)
         #     time.sleep(0.2)
 
-        vals = angles_ser.readline().decode("utf-8").strip()
-        try:
-            _, lenc, renc = vals.split(",")
-            print("encoder left", vals)
-        except Exception:
-            print("Error reading")
-            time.sleep(0.1)
-            continue
-        if int(lenc) > 5760:
-            print("already moving setting speed to 90")
-            speed = 90
-            break
+        for i in range(100):
+            angles_ser.flushInput()
+            vals = angles_ser.readline().decode("utf-8").strip()
+            try:
+                _, lenc, renc = vals.split(",")
+                print("encoder left", vals)
+            except Exception:
+                print("Error reading")
+                time.sleep(0.1)
+                continue
+            if int(lenc) > 5760:
+                print("already moving setting speed to 90")
+                speed = 90
+                break
+            time.sleep(0.05)
         # time.sleep(3)
         # motor_ser.write("0,0\n".encode())
         # time.sleep(4)
