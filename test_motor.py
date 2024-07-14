e1270e29808918e6995c5b5515f2093e9eb68ee5
ValeriaCampagna <valeria.campagna1@gmail.com>
2024-07-14 17:03:22 -0400

    Update test agggagin

--- test_motor.py
+++ test_motor.py
@@ -9,6 +9,12 @@ try:
     while True:
         speed = 0
         for i in range(20):
+            command = f"{speed},{speed}\n"
+            motor_ser.write(command.encode())
+            speed = min(155, speed+8)
+            print("current speed:", speed)
+            time.sleep(0.2)
+        for i in range(100):
             vals = angles_ser.readline().decode("utf-8").strip()
             try:
                 _, lenc, renc = vals.split(",")
@@ -17,15 +23,11 @@ try:
                 time.sleep(0.1)
                 continue
             if int(lenc) > 5760:
-                print("already moving")
+                print("already moving setting speed to 90")
                 speed = 90
                 break
-            command = f"{speed},{speed}\n"
-            motor_ser.write(command.encode())
-            speed = min(180, speed+10)
-            print("current speed:", speed)
-            time.sleep(0.2)
-        time.sleep(4)
+            time.sleep(0.05)
+        time.sleep(3)
         motor_ser.write("0,0\n".encode())
         time.sleep(4)
 except KeyboardInterrupt:
