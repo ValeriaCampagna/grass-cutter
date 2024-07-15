6f7147155c8555072a52ca0ead7655a404bedcdb
ValeriaCampagna <valeria.campagna1@gmail.com>
2024-07-14 21:29:06 -0400

    Update u_turn

--- big_dick_controller.py
+++ big_dick_controller.py
@@ -206,10 +206,10 @@ class RobotController:
             # IF angle is positive stop right wheel and increase left wheel speed
             if self.sensor_data["angle"] > self.target_angle:
                 # print("Turning Right")
-                self.send_speed(255, -100)
+                self.send_speed(self.TURNING_SPEED, -(self.TURNING_SPEED//2))
             elif self.sensor_data["angle"] < self.target_angle:
                 # print("Turning Left")
-                self.send_speed(-100, 255)
+                self.send_speed(-(self.TURNING_SPEED//2), self.TURNING_SPEED)
             time.sleep(1)
         return deviation
 
