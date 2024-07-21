72f34b1b03801dc4cdaaea3794157fdfd5ddcdce
ValeriaCampagna <valeria.campagna1@gmail.com>
2024-07-21 12:56:52 -0400

    Update boost

--- big_dick_controller.py
+++ big_dick_controller.py
@@ -464,7 +464,7 @@ def boost_state(controller: RobotController):
     if controller.cached_speeds == (0, 0):
         controller.distance_after_encoder_reset = controller.get_tracked_distance()
         controller.cached_speeds = (controller.LEFT_CRUISE_SPEED, controller.RIGHT_CRUISE_SPEED)
-    print("Boost distance: ", controller.sensor_data["left_encoder"], controller.total_ticks_left)
+    print("Boost distance: ", controller.get_tracked_distance(), controller.sensor_data["left_encoder"])
     if (t := (controller.get_tracked_distance() - controller.distance_after_encoder_reset)) < 30:
         controller.LEFT_CRUISE_SPEED = min(255, controller.LEFT_CRUISE_SPEED + increase)
         controller.RIGHT_CRUISE_SPEED = min(255, controller.RIGHT_CRUISE_SPEED + increase)
