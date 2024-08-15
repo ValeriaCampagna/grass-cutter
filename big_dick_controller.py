935cf2378e79059fb2fdfa670febcea2c219ccd6
ValeriaCampagna <valeria.campagna1@gmail.com>
2024-08-14 21:18:51 -0400

    Update reset_encoders again again

--- big_dick_controller.py
+++ big_dick_controller.py
@@ -319,9 +319,9 @@ class RobotController:
 
     def reset_encoders(self):
         self.send_speed(0, 0)
-        # time.sleep(0.5)
         self.total_ticks_left = self.sensor_data["left_encoder_raw"]
         self.total_ticks_right = self.sensor_data["right_encoder_raw"]
+        time.sleep(0.5)
         logging.info(f"Total Encoder Ticks: L {self.total_ticks_left} | R {self.total_ticks_right}")
 
     def reset_angle(self):
