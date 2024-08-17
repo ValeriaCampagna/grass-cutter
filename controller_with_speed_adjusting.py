bceefb6c316885bf88f501834cc8483981d23a69
ValeriaCampagna <valeria.campagna1@gmail.com>
2024-08-17 15:35:23 -0400

    update speed

--- controller_with_speed_adjusting.py
+++ controller_with_speed_adjusting.py
@@ -125,9 +125,9 @@ class ObstacleDetectionRoutine:
 
 class RobotController:
     def __init__(self):
-        self.TURNING_SPEED = 10
-        self.LEFT_CRUISE_SPEED = 8
-        self.RIGHT_CRUISE_SPEED = 8
+        self.TURNING_SPEED = 25
+        self.LEFT_CRUISE_SPEED = 22
+        self.RIGHT_CRUISE_SPEED = 22
 
         # In Centimeters
         self.WHEEL_RADIUS = 44
