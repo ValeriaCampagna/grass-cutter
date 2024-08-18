2709d1652dc55c058088661b7a8c66628e8cac0b
ValeriaCampagna <valeria.campagna1@gmail.com>
2024-08-18 17:23:20 -0400

    Update right wheel speed

--- controller_with_speed_adjusting.py
+++ controller_with_speed_adjusting.py
@@ -127,7 +127,7 @@ class RobotController:
     def __init__(self):
         self.TURNING_SPEED = 10 #20
         self.LEFT_CRUISE_SPEED = 10 #22
-        self.RIGHT_CRUISE_SPEED = 9 #22
+        self.RIGHT_CRUISE_SPEED = 10 #22
 
         # In Centimeters
         self.WHEEL_RADIUS = 44
