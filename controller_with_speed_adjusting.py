34b21434947a4cd04698542941f18e0f91ae9355
ValeriaCampagna <valeria.campagna1@gmail.com>
2024-09-24 20:00:13 -0400

    Update forward to adjust direction correctly

--- controller_with_speed_adjusting.py
+++ controller_with_speed_adjusting.py
@@ -311,8 +311,12 @@ class RobotController:
         if deviation > self.angle_error_margin:
             # self.adjusting_angle = True
             if real_angle > self.target_angle:
-                print("Turning left, angle:", real_angle)
-                self.send_speed(0, self.RIGHT_CRUISE_SPEED)
+                if self.target_angle == 0:
+                    print("Target is 0 turining right, angle:", real_angle)
+                    self.send_speed(self.LEFT_CRUISE_SPEED, 0)
+                else:
+                    print("Turning left, angle:", real_angle)
+                    self.send_speed(0, self.RIGHT_CRUISE_SPEED)
             elif real_angle < self.target_angle:
                 print("Turning right, angle:", real_angle)
                 self.send_speed(self.LEFT_CRUISE_SPEED, 0)
