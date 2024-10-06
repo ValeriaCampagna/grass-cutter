e8bf8143e60e437f601397343a5801cc1b7dee8d
ValeriaCampagna <valeria.campagna1@gmail.com>
2024-10-06 16:50:20 -0400

    Update adjust

--- controller_with_speed_adjusting.py
+++ controller_with_speed_adjusting.py
@@ -575,7 +575,7 @@ def adjust_state(controller: RobotController):
         last_check = time.time()
         controller.angle_error_margin = 0
         min_turning_speed = round(cached_turning_speed * 0.4)
-        controller.TURNING_SPEED = int(controller.TURNING_SPEED * 0.7)
+        controller.TURNING_SPEED = int(controller.TURNING_SPEED * 0.8)
 
     deviation = controller.axis_turn()
     real_angle = 0 if (x:=controller.sensor_data["angle"]) == 360 else x
